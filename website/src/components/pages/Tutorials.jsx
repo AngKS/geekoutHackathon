b725803c6a4d9f5a1957b81f12f68aeedb185d18
Nathania02 <1901030E@student.tp.edu.sg>
2021-06-25 11:11:19 +0800

    update techniques

--- website/src/components/pages/Tutorials.jsx
+++ website/src/components/pages/Tutorials.jsx
@@ -12,11 +12,11 @@ function Tutorials() {
                 <div className="col-md-3">
                     <Sidebar1 className="col-md-4"/>
                 </div>
-                <div className="col-md-6 mx-auto">
-                    <div className="row">
+                <div className="col-md-8 mx-auto">
+                    <div className="row" id="intro">
                         <Intro className="col-md-6"/>
                     </div>
-                    <div className="row mt-5">
+                    <div className="row mt-5"  id="text">
                         <Techniquecard className="w-100"/>
                     </div>
                 </div>
