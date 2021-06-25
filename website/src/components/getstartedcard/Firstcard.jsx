b8e4adff9dba8dfc958a467008e4c46f82b4292d
Tan Shi Wei Cody <72619037+A-Cup-Of-Coffee-Cody@users.noreply.github.com>
2021-06-25 09:22:35 +0800

    update_getstarted

--- website/src/components/getstartedcard/Firstcard.jsx
+++ website/src/components/getstartedcard/Firstcard.jsx
@@ -5,7 +5,7 @@ import './cards.css'
 function Firstcard() {
     return (
         <div className='Getstart'>
-            <div className="Getstart_content card text-center">
+            <div className="Getstart_content Getstart_transit card text-center">
                 <h5 className="Getstart_title card-title my-5">WELCOME<br />to the <cite>GEEKIE</cite> world of CODING!</h5>
                 <Link to="/getStarted_2"><button className="Getstart_button btn btn-lg">Click to begin</button></Link>
             </div>
