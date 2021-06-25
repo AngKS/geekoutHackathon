f9956b6f0e3e0c1fb26bf7887aee8fc4334c5051
AKS <60446729+AngKS@users.noreply.github.com>
2021-06-25 12:26:05 +0800

    fixed articles links and touched up on resources layout

--- website/src/components/pages/Resources.jsx
+++ website/src/components/pages/Resources.jsx
@@ -12,26 +12,28 @@ function Resources() {
     return (
         <div>
             <Navbar />
-            <h1 className="text-center text-dark didac-font">Languages</h1>
-            <div className="row">
-                {
-                    allCourses.map(item => <Coursecard data={item}/>)
-                }
-                </div>
-                <br></br>
-            <h1 className="text-center text-dark didac-font">Computational Thinking Courses</h1><br></br>
-            <h2 className="text-center railway-font">
-            Computational thinking is the process of approaching a problem in a systematic manner and creating
-            and expressing a solution such that it can be carried out by a computer.
-            Many quantitative and data-centric problems can be solved using computational thinking and
-            an understanding of computational thinking will give you a foundation for solving problems that have real-world,
-            social impact.  
-            </h2>
-            <div className="row">
-                {
-                    allCT.map(item => <CTcard data={item}/>)
-                }
-                </div>
+            <section className="p-2 container">
+                <h1 className="text-center text-dark didac-font">Languages</h1>
+                <div className="row">
+                    {
+                        allCourses.map(item => <Coursecard data={item}/>)
+                    }
+                    </div>
+                    <br></br>
+                <h1 className="text-center text-dark didac-font">Computational Thinking Courses</h1><br></br>
+                <h6 className="text-left railway-font my-2">
+                Computational thinking is the process of approaching a problem in a systematic manner and creating
+                and expressing a solution such that it can be carried out by a computer.
+                Many quantitative and data-centric problems can be solved using computational thinking and
+                an understanding of computational thinking will give you a foundation for solving problems that have real-world,
+                social impact.  
+                </h6>
+                <div className="row mt-3">
+                    {
+                        allCT.map(item => <CTcard data={item}/>)
+                    }
+                    </div>
+            </section>
         </div>
     )
 }
